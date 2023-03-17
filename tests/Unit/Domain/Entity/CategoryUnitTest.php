b7345c7e68e650116c1fb61f467052f1ff8134ee
Luiz Eduardo <luiz.silva@fretebras.com.br>
2023-03-17 14:49:50 -0300

    feat: category entity activation and deactivation

--- tests/Unit/Domain/Entity/CategoryUnitTest.php
+++ tests/Unit/Domain/Entity/CategoryUnitTest.php
@@ -10,14 +10,40 @@ class CategoryUnitTest extends TestCase
     public function testAttributes(): void
     {
         $category = new Category(
-            id: '123',
             name: 'new cat',
             description: 'new desc',
             isActive: true,
         );
-        
+
         $this->assertEquals('new cat', $category->name);
         $this->assertEquals('new desc', $category->description);
         $this->assertEquals(true, $category->isActive);
+    }
+
+    public function testActivated(): void
+    {
+        $category = new Category(
+            name: 'new cat',
+            isActive: false,
+        );
+
+        $this->assertFalse($category->isActive);
+
+        $category->activate();
+
+        $this->assertTrue($category->isActive);
+    }
+
+    public function testDeactivated(): void
+    {
+        $category = new Category(
+            name: 'new cat',
+        );
+
+        $this->assertTrue($category->isActive);
+
+        $category->deactivate();
+
+        $this->assertFalse($category->isActive);
     }
 }
