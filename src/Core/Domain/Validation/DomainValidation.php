f68f8409c10d4460d38322b7169429f9191f3321
Luiz Eduardo <luiz.silva@fretebras.com.br>
2023-03-17 15:27:28 -0300

    feat: domain validations

--- src/Core/Domain/Validation/DomainValidation.php
+++ src/Core/Domain/Validation/DomainValidation.php
@@ -6,10 +6,24 @@ use Core\Domain\Exception\EntityValidationException;
 
 class DomainValidation
 {
-    public static function notNull(string $value, string $expectionMessage = null): void
+    public static function notNull(string $value, ?string $expectionMessage = null): void
     {
         if (empty($value)) {
             throw new EntityValidationException($expectionMessage ?? 'Should not be empty');
+        }
+    }
+
+    public static function stringMaxLength(string $value, int $maxLength = 255, ?string $expectionMessage = null): void
+    {
+        if (strlen($value) >= $maxLength) {
+            throw new EntityValidationException($expectionMessage ?? 'Should not be greater then ' . $maxLength);
+        }
+    }
+
+    public static function stringMinLength(string $value, int $minLength = 2, ?string $expectionMessage = null): void
+    {
+        if (strlen($value) <= $minLength) {
+            throw new EntityValidationException($expectionMessage ?? 'Should not be greater then ' . $minLength);
         }
     }
 }
--- tests/Unit/Domain/Validation/DomainValidationUnitTest.php
+++ tests/Unit/Domain/Validation/DomainValidationUnitTest.php
@@ -11,8 +11,37 @@ class DomainValidationUnitTest extends TestCase
     public function testNotNull(): void
     {
         $value = '';
+
+        $this->expectException(EntityValidationException::class);
+
+        DomainValidation::notNull($value);
+    }
+
+    public function testNotNullWithCustomExceptionMessage(): void
+    {
+        $value = '';
         
         $this->expectException(EntityValidationException::class);
-        DomainValidation::notNull($value);
+        $this->expectExceptionMessage('custom error');
+
+        DomainValidation::notNull($value, 'custom error');
+    }
+
+    public function testStringMaxLength(): void
+    {
+        $value = 'tes';
+
+        $this->expectException(EntityValidationException::class);
+
+        DomainValidation::stringMaxLength($value, 3, 'custom error');
+    }
+
+    public function testStringMinLength(): void
+    {
+        $value = 'test';
+
+        $this->expectException(EntityValidationException::class);
+
+        DomainValidation::stringMinLength($value, 8, 'custom error');
     }
 }
